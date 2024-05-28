c18d0af1c1063ece5220e2eab9198faa6b0b30a1
juliaooliveira <148073511+juliaooliveira@users.noreply.github.com>
2024-05-28 10:23:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,81 @@
+const caixaPrincipal=document.querySelector(".caixa-principal);
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado);
 
+const perguntas=[
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "Alternativa 2"
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas:[
+      "Alternativa 1"
+      "Alternativa 2"
+    ]
+  },
+];
+
+let atual=0;
+let perguntasAtual;
+
+function mostraPergunta (){
+  perguntaAtual=perguntas [atual];
+  caixaPerguntas.textContent=perguntaAtual.enunciado;
+}
+
+mostraPergunta();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];  
